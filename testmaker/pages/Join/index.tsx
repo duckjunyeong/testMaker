33536e2b04b19967da0d609cd25f18a9573e238f
AnJunYeong <whwnsdud4592@naver.com>
2023-10-09 23:24:58 +0900

    회원가입 구현

--- testmaker/pages/Join/index.tsx
+++ testmaker/pages/Join/index.tsx
@@ -1,15 +1,41 @@
-import React from "react";
-
+import React, {useCallback, useState} from "react";
+import axios from "axios";
 
 const Join = () => {
+const [email, setEmail] = useState('');
+const [nickname, setNickname] = useState('');
+const [password, setPassword] = useState('');
 
+const createUser = useCallback((e)=>{
+    e.preventDefault();
+    console.log(email, nickname, password)
+    axios.post("/api/user", {email,nickname, password})
+    .then((response) => {   
+        console.log(response)
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+} , [email,password,nickname])
+
+const changeEmail = useCallback((e) => {
+    setEmail(e.target.value)
+}, [email]);
+
+const changeNickname = useCallback((e) => {
+    setNickname(e.target.value)
+}, [nickname]);
+
+const changePassword = useCallback((e) => {
+    setPassword(e.target.value)
+}, [password]);
 
     return(
         <>
-            <form action="POST">
-                <input type="email" placeholder="email" name="email"></input>
-                <input type="text"  placeholder="nickname" name='nickname'/>
-                <input type="password" placeholder="password"  name='password'/>
+            <form onSubmit={createUser}>
+                <input type="email" placeholder="email" id="email" name="email" value={email} onChange={changeEmail}></input>
+                <input type="text"  placeholder="nickname" id="nickname" name='nickname' value={nickname} onChange={changeNickname}/>
+                <input type="password" placeholder="password"  id="password" name='password' value={password} onChange={changePassword}/>
                 <button>회원가입하기</button>
             </form>        
         </>
--- testmaker/webpack.config.ts
+++ testmaker/webpack.config.ts
@@ -80,15 +80,15 @@ const config: Configuration = {
   },
   devServer: {
     historyApiFallback: true, // react router
-    port: 3090,
+    port: 3080,
     devMiddleware: { publicPath: '/dist/' },
     static: { directory: path.resolve(__dirname) },
-    // proxy: {
-    //   '/api/': {
-    //     target: 'http://localhost:3095',
-    //     changeOrigin: true,
-    //   }
-    // }
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:3085',
+        changeOrigin: true,
+      }
+    }
   },
 };
 
