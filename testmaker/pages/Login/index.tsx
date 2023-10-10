ef3fb08abfb2aad799c42c6983f9714eb0d89873
AnJunYeong <whwnsdud4592@naver.com>
2023-10-10 11:55:04 +0900

    로그인 구현

--- testmaker/pages/Login/index.tsx
+++ testmaker/pages/Login/index.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useCallback, useState } from "react";
 
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
+  const changeEmail = useCallback((e) => {
+    setEmail(e.target.value)
+}, [password]);
+
+  const changePassword = useCallback((e) => {
+    setPassword(e.target.value)
+}, [password]);
+
+  const checkLogin = useCallback((e) =>{
+    e.preventDefault();
+    axios.post("/api/user/login", {email,password})
+    .then((response) => {
+        console.log(response.data.nickname)
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+  } , [email, password])
 
     return(
         <>
-            <form action="POST">
-                <input type="email" name="email"/>
-                <input type="password" name="password" />
+            <form onSubmit={checkLogin}>
+                <input type="email" id="email" name="email" onChange={changeEmail}/>
+                <input type="password"  id="password"name="password" onChange={changePassword}/>
                 <button>로그인하기</button>
             </form>
                 <a href='/join'>회원가입</a>
